38134f138f685954f703eaa39bb2c0979924c299
EduardARMENIA <eduardghahramanyan78@gmail.com>
2023-01-24 11:22:52 +0400

    first commit

---
+++ components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form">
+    <form @submit.prevent>
+      <input type="text" v-model="name" placeholder="name" />
+      <input type="text" v-model="email" placeholder="Email" />
+      <button type="submit" @click="submitData">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  methods: {
+    submitData(e) {
+      this.$emit("passData", { name: this.name, emial: this.email });
+    },
+  },
+};
+</script>
